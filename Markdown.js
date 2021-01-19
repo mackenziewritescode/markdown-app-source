147e26212bc7407fd64ad34343a32a11fc3e7957
mackenziewritescode <mackenzie.charlton@gmail.com>
2021-01-19 17:15:49 -0500

    moved placeholder

--- Markdown.js
+++ Markdown.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Markdown.scss";
 import marked from "marked";
 import { RiArrowGoBackLine } from "react-icons/ri";
+import placeholder from "./placeholder";
 
 marked.setOptions({
   breaks: true,
@@ -14,8 +15,8 @@ function Markdown() {
   const [previewBtn, setPreviewBtn] = useState(false);
 
   useEffect(() => {
-    document.title = "Markdown Preview."
-  })
+    document.title = "Markdown Preview.";
+  });
 
   function setText(event) {
     setInputText(event.target.value);
@@ -29,7 +30,6 @@ function Markdown() {
     previewBtn ? setPreviewBtn(false) : setPreviewBtn(true);
   }
 
-  // instead have a expanded class, or a suffix like 'container-expanded'
   let classes = [];
 
   if (editorBtn === true || previewBtn === true) {
@@ -52,7 +52,7 @@ function Markdown() {
 
   return (
     <div>
-      <a className="portfolio" href="https://www.sunkenworld.com/">
+      <a className="portfolio" href="https://www.sunkenworld.com/#page-3">
         <p className="portfolio-text">
           <RiArrowGoBackLine /> Back to portfolio
         </p>
@@ -112,48 +112,5 @@ function Markdown() {
     </div>
   );
 }
-
-const placeholder = `# This is Markdown Preview.
-
-## A little site made using React.
-### It uses Marked.js.
-
-It uses the markup language **Markdown** to... well, ~~markdown~~ _markup_ the text.
-
-You can do all sorts of things, like:
-
-\`\`\`
-    write code between backticks.
-\`\`\`
-
-It's also good for
-  - Making
-  - Lists
-    - Like
-    - This
-
-Hey. | Why not | Make a table?
-------| ------- | ---------
-Put whatever | you want | in here.
-Or leave it empty. | ... |
-
-
-While I have your attention, here's a bit of info on this little app inside of a block quote:
-
-> Everything on this page was coded by hand. No copying and pasting in site.
->
-> Inputted text gets updated live in the Preview section through the use of React and the library Marked.
->
-> The layout of this app was designed using CSS grids for a clean look and responsive layout. If possible, try resizing the window to see how the headers snap from being written vertically and to the left of the content to being on top when the viewport gets narrow.
->
-> The buttons used to expand and collapse the windows use React's state to set the classes of various elements, as well as change the grid layout.
-
-
-By the way, here's a link to [my homepage](https://www.sunkenworld.com/).
-
-And finally, here's an embedded image:
-
-![Triforce](http://www.blog.sunkenworld.com/wp-content/uploads/triforce.png)
-`;
 
 export default Markdown;
---
+++ placeholder.js
@@ -0,0 +1,44 @@
+const placeholder = `# This is Markdown Preview.
+
+## A little site made using React.
+### It uses Marked.js.
+
+It uses the markup language **Markdown** to... well, ~~markdown~~ _markup_ the text.
+
+You can do all sorts of things, like:
+
+\`\`\`
+    write code between backticks.
+\`\`\`
+
+It's also good for
+  - Making
+  - Lists
+    - Like
+    - This
+
+Hey. | Why not | Make a table?
+------| ------- | ---------
+Put whatever | you want | in here.
+Or leave it empty. | ... |
+
+
+While I have your attention, here's a bit of info on this little app inside of a block quote:
+
+> Everything on this page was coded by hand. No copying and pasting in site.
+>
+> Inputted text gets updated live in the Preview section through the use of React and the library Marked.
+>
+> The layout of this app was designed using CSS grids for a clean look and responsive layout. If possible, try resizing the window to see how the headers snap from being written vertically and to the left of the content to being on top when the viewport gets narrow.
+>
+> The buttons used to expand and collapse the windows use React's state to set the classes of various elements, as well as change the grid layout.
+
+
+By the way, here's a link to [my homepage](https://www.sunkenworld.com/).
+
+And finally, here's an embedded image:
+
+![Triforce](http://www.blog.sunkenworld.com/wp-content/uploads/triforce.png)
+`;
+
+export default placeholder;
